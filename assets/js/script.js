367c1a91ec9bfe97bf82f632c22b7ce9cb98cbda
zanderson1998 <zanderson1998@yahoo.com>
2020-12-06 21:02:32 -0700

    add color coding with respect to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,221 @@
-$(".text-input-box").on("click",function() {
+//set day at top of page
+var currentDay = moment().format("dddd MMMM Do, YYYY");
+$(".current-date").append(currentDay);
+
+//change background colors based on if the time is passed or not
+var time = new Date().getHours();
+console.log(time);
+
+function keepTrack() {
+    if (time < 10){
+        $(".9").addClass("bg-warning");
+        $(".10").addClass("bg-success");
+        $(".11").addClass("bg-success");
+        $(".12").addClass("bg-success");
+        $(".1").addClass("bg-success");
+        $(".2").addClass("bg-success");
+        $(".3").addClass("bg-success");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 11) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-warning");
+        $(".11").addClass("bg-success");
+        $(".12").addClass("bg-success");
+        $(".1").addClass("bg-success");
+        $(".2").addClass("bg-success");
+        $(".3").addClass("bg-success");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 12) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-warning");
+        $(".12").addClass("bg-success");
+        $(".1").addClass("bg-success");
+        $(".2").addClass("bg-success");
+        $(".3").addClass("bg-success");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 13) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-warning");
+        $(".1").addClass("bg-success");
+        $(".2").addClass("bg-success");
+        $(".3").addClass("bg-success");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 14) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-secondary");
+        $(".1").addClass("bg-warning");
+        $(".2").addClass("bg-success");
+        $(".3").addClass("bg-success");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 15) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-secondary");
+        $(".1").addClass("bg-secondary");
+        $(".2").addClass("bg-warning");
+        $(".3").addClass("bg-success");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 16) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-secondary");
+        $(".1").addClass("bg-secondary");
+        $(".2").addClass("bg-secondary");
+        $(".3").addClass("bg-warning");
+        $(".4").addClass("bg-success");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 17) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-secondary");
+        $(".1").addClass("bg-secondary");
+        $(".2").addClass("bg-secondary");
+        $(".3").addClass("bg-secondary");
+        $(".4").addClass("bg-warning");
+        $(".5").addClass("bg-success");
+    }
+    else if (time < 18) {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-secondary");
+        $(".1").addClass("bg-secondary");
+        $(".2").addClass("bg-secondary");
+        $(".3").addClass("bg-secondary");
+        $(".4").addClass("bg-secondary");
+        $(".5").addClass("bg-warning");
+    }
+    else {
+        $(".9").addClass("bg-secondary");
+        $(".10").addClass("bg-secondary");
+        $(".11").addClass("bg-secondary");
+        $(".12").addClass("bg-secondary");
+        $(".1").addClass("bg-secondary");
+        $(".2").addClass("bg-secondary");
+        $(".3").addClass("bg-secondary");
+        $(".4").addClass("bg-secondary");
+        $(".5").addClass("bg-secondary");
+    }
+};
+
+//editing text boxes
+$(".text-input-box").on("click", function () {
     console.log("clicked");
     var text = $(this).find("p")
         .text()
         .trim();
     var textInput = $("<textarea>")
-        .addClass("text")
+        //.addClass(this.className)
         .val(text);
     $(this).find("p").replaceWith(textInput);
     textInput.trigger("focus");
 });
 
-$(".text-input-box").on("blur","textarea",function(){
+$(".text-input-box").on("blur", "textarea", function () {
     var text = $(this)
         .val()
         .trim();
     var taskP = $("<p>")
-        .addClass("info")
+        //.addClass(this.className,"fill-in")
         .text(text);
     $(this).replaceWith(taskP);
 });
 
+// function editBox(event){
+    
+// }
+
+
+//document.querySelector(".btn-save").addEventListener("click",editBox);
+
+
 // save/load functions
 var stuffToDo = {};
-var saveTasks = function() {
+var saveTasks = function () {
+    if (!stuffToDo) {
+        stuffToDo = {
+            nine: [],
+            ten: [],
+            eleven: [],
+            twelve: [],
+            one: [],
+            two: [],
+            three: [],
+            four: [],
+            five: []
+        };
+    }
     //stuffToDo = $(".text-input-box").text();
 
-    stuffToDo.nine = $(".nineAM").val();
+    stuffToDo.nine = document.getElementById("nineAM").textContent;
+    stuffToDo.ten = document.getElementById("tenAM").textContent;
+    stuffToDo.eleven = document.getElementById("elevenAM").textContent;
+    stuffToDo.twelve = document.getElementById("twelveAM").textContent;
+    stuffToDo.one = document.getElementById("oneAM").textContent;
+    stuffToDo.two = document.getElementById("twoAM").textContent;
+    stuffToDo.three = document.getElementById("threeAM").textContent;
+    stuffToDo.four = document.getElementById("fourAM").textContent;
+    stuffToDo.five = document.getElementById("fiveAM").textContent;
 
-    localStorage.setItem("stuffToDo",JSON.stringify(stuffToDo));
+    localStorage.setItem("stuffToDo", JSON.stringify(stuffToDo));
 };
 
-var loadTasks = function(){
-    stuffToDo = JSON.parse(localStorage.getItem("stuffToDo"));
-    
-    //$(".text-input-box").text(stuffToDo);
-    $(".nineAM").text(stuffToDo.nine);
+var loadTasks = function () {
 
-    // for (var i = 1; i < 10; i++){
-    //     var things = stuffToDo[i];
-    //     $(".id-" + [i]).text(stuffToDo[i])
-    //     // var things = $("<p>")
-    //     //     .addClass("id-" + [i])
-    //     //     .text(stuffToDo[i])
-    //     //$(".id-" + [i]).text(stuffToDo[i]);
-    // } 
+    if (!stuffToDo) {
+        stuffToDo = {
+            nine: [],
+            ten: [],
+            eleven: [],
+            twelve: [],
+            one: [],
+            two: [],
+            three: [],
+            four: [],
+            five: []
+        };
+    }
+
+    stuffToDo = JSON.parse(localStorage.getItem("stuffToDo"));
+
+    //document.getElementById("nineAM").textContent = stuffToDo.nine;
+    $(".nineAM").text(stuffToDo.nine);
+    $(".tenAM").text(stuffToDo.ten);
+    $(".elevenAM").text(stuffToDo.eleven);
+    $(".twelveAM").text(stuffToDo.twelve);
+    $(".oneAM").text(stuffToDo.one);
+    $(".twoAM").text(stuffToDo.two);
+    $(".threeAM").text(stuffToDo.three);
+    $(".fourAM").text(stuffToDo.four);
+    $(".fiveAM").text(stuffToDo.five);
+
 }
 
 loadTasks();
 
-//setInterval(function() {
-//    saveTasks();
-//},5000);
+setInterval(function() {
+   saveTasks();
+   keepTrack();
+},1000);
 //console.log($(".text-input-box").text());
